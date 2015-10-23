00a4bc272f09d1ee0e7102a30f1e31c4945e7230
Brian Porter <beporter@users.sourceforge.net>
2015-10-23 12:06:26 -0500

    Add UuidShell.

---
+++ src/Shell/UuidShell.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * UuidShell
+ *
+ * @package UuidShell\Shell
+ */
+namespace UuidShell\Shell;
+
+use Cake\Console\Shell;
+use Cake\Utility\Text;
+
+/**
+ * UuidShell class.
+ *
+ * Provides a command-line shortcut for generating batches of new UUIDs.
+ */
+class UuidShell extends Shell {
+
+	/**
+	 * Overrides the default welcome function in order to suppress the
+	 * normal "Welcome to CakePHP" banner.
+	 *
+	 * @return void
+	 */
+	public function _welcome() {
+		// Do nothing.
+	}
+
+	/**
+	 * Generate a batch of new UUIDs.
+	 *
+	 * @return	void
+	 */
+	public function main() {
+		// @codingStandardsIgnoreStart
+		for (
+			$count = (isset($this->args[0]) ? max(1, (int)$this->args[0]) : 1);
+			$count-- > 0;
+			$this->out(Text::uuid())
+		);
+		// @codingStandardsIgnoreEnd
+	}
+
+	/**
+	 * getOptionParser
+	 *
+	 * Processing command line options.
+	 *
+	 * @access public
+	 * @return CosnsoleOptionParser
+	 * @codeCoverageIgnore
+	 */
+	public function getOptionParser() {
+		$parser = parent::getOptionParser();
+		$parser
+			->addArgument('count', [
+				'help' => 'Optional positive integer indicating how many UUIDs to generate and print. (Default: 1)',
+				'required' => false,
+			])
+			->description(__('Generates batches of UUIDs using Cake\'s String class.'));
+		return $parser;
+	}
+}
